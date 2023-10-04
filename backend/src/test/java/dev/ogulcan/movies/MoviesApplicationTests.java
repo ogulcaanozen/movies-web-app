556d21dcdfdbafc8cd85c648a929f2976bc3b35b
Oğulcan Özen <44923875+ogulcaanozen@users.noreply.github.com>
2023-10-04 17:34:30 +0300

    Add files via upload

---
+++ backend/src/test/java/dev/ogulcan/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ogulcan.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
